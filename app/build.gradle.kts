0c253b8fb487409e4425fa34a0bec1f5c5b3e07c
HaiKM <haikm@apero.vn>
2024-12-17 16:31:32 +0700

    Add file check PR

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.apero.ci_cd_github_actions"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.apero.ci_cd_github_actions"
         minSdk = 24
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
